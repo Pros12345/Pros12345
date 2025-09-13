e80e525866c45d0c2526878d637681bd6eaa8476
Pros12345 <prosenjitmaigram@gmail.com>
2025-09-13 19:18:58 +0530

    created registration

--- src/app/components/register.component/register.component.ts
+++ src/app/components/register.component/register.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register.component',
-  imports: [],
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registrationForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      firstName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registrationForm.valid) {
+      console.log('Registration data:', this.registrationForm.value);
+      alert('Registration successful!');
+      this.registrationForm.reset();
+    } else {
+      console.log('Form is invalid.');
+    }
+  }
 }
